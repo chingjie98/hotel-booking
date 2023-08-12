470c61e00541a664eacb31a9753b18931c659215
chingjie98 <35895182+chingjie98@users.noreply.github.com>
2023-08-12 19:09:01 +0800

    Added in routers, and basic routing paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Hotel from './pages/hotel/Hotel'
+import List from './pages/list/List'
 
 
 function App() {
 
   return (
-    <>
-      Hello World!
-    </>
+    <BrowserRouter>
+      <main>
+        <Routes>
+          <Route path = "/" element = {<Home/>}></Route>
+          <Route path = "/hotel" element = {<Hotel/>}></Route>
+          <Route path = "/list" element = {<List/>}></Route>
+        </Routes>
+      </main>
+    </BrowserRouter>
   )
 }
 
