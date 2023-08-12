e7f700f1cbeb64ae514f19a6d259e248bfd55dcc
chingjie98 <35895182+chingjie98@users.noreply.github.com>
2023-08-12 23:16:14 +0800

    added in navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar">
+        AIRBNB
+        <div className="yolo">
+          <button>LOGIN</button>
+          <button>REGISTER</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
